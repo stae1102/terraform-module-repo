37f695d493e6bc95f3ebd3b5700e7dcd0f12d029
Seongtae <stae1102@gmail.com>
2023-12-22 15:04:53 +0900

    feat: add aws variable

---
+++ terraform-aws-ec2/variable.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "vm 인스턴스 타입 정의"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "vm 인스턴스 이름 정의"
+  default     = "my_ec2"
+}
